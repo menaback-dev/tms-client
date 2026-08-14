762c08e513fd3fc980eb07ec29262361629ee638
menadev <abdullahtate3@gmail.com>
2026-08-15 02:40:44 +0300

    Refactor: Fix some configs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject , OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EnrollmentStore } from '../app/store/enrollment.store';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('tms-client');
+export class App implements OnInit {
+  private store = inject(EnrollmentStore);
+  ngOnInit() {
+    this.store.loadEnrollments();
+    this.store.listenForLiveUpdates();
+}
 }
